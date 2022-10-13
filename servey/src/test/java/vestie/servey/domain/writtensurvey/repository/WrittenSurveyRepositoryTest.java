e9465e61817321e470321b40f7888cbaf05681c4
shinD <huipulci1@naver.com>
2022-10-14 02:13:04 +0900

    [TODO] [#4] WrittenSurvey와 ChoiceQuestion에 대해 cascade = ALL 테스트 작성해야 함

---
+++ servey/src/test/java/vestie/servey/domain/writtensurvey/repository/WrittenSurveyRepositoryTest.java
@@ -0,0 +1,24 @@
+package vestie.servey.domain.writtensurvey.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/**
+ * Created by ShinD on 2022/10/14.
+ */
+@DataJpaTest
+class WrittenSurveyRepositoryTest {
+
+	@Test
+	@DisplayName("설문 작성 시, 답변들까지 한번에 저장")
+	public void successSavedWrittenSurveyAndAnswers() throws Exception {
+	    //given
+		throw new RuntimeException("TODO : 설문 작성 시, 답변들까지 한번에 저장");
+	    //when
+
+	    //then
+
+	}
+
+}
