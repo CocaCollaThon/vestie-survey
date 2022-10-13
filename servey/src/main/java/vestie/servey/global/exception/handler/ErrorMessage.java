af1969fa61916ce9b283dedaa5a05f90072282f4
GwakMirae <alfo9490@naver.com>
2022-10-14 00:37:46 +0900

    [FEAT] [#7] ArgumentResolver 구현

---
+++ servey/src/main/java/vestie/servey/global/exception/handler/ErrorMessage.java
@@ -0,0 +1,19 @@
+package vestie.servey.global.exception.handler;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorMessage {
+    @Builder.Default
+    private String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+    private int status;
+    private int errorCode;
+    private String message;
+}
