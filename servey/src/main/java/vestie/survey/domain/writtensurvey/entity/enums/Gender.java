8c7f70c6d8bfbe10e60f0a8bb60989fd2e8d081d
shinD <huipulci1@naver.com>
2022-10-15 02:32:04 +0900

    [REFACTOR] Enum 세팅 시 반복문 -> valueOf로 변경

--- servey/src/main/java/vestie/survey/domain/writtensurvey/entity/enums/Gender.java
+++ servey/src/main/java/vestie/survey/domain/writtensurvey/entity/enums/Gender.java
@@ -12,11 +12,6 @@ public enum Gender {
 
 	@JsonCreator
 	public static Gender from(String gender){
-		for(Gender g : Gender.values()){
-			if(g.name().equals(gender)){
-				return g;
-			}
-		}
-		return null;
+		return valueOf(gender);
 	}
 }
