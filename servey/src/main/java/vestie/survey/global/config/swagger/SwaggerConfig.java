5762b9d9d2e3649ab9dc009c9d3206b263db957e
Mirae Kwak <alfo9490@naver.com>
2022-11-08 22:11:55 -0800

    [FEAT] [#23] swagger 설정 (#24)

---
+++ servey/src/main/java/vestie/survey/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package vestie.survey.global.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Vestie Survey API Doc")
+                .description("vestie survey api 문서화")
+                .version("3.0")
+                .build();
+    }
+
+}
